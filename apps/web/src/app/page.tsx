8ca7b19fbbcad7b51eb37c779359f256528871de
Jean P.D. Meijer <jean@analoginterface.io>
2025-05-23 13:54:10 +0200

    chore(web): setup temporary redirect to repo

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { auth } from "@repo/auth/server";
 
 export default async function Home() {
-  const session = await auth.api.getSession({ headers: await headers() });
-
-  if (!session) {
-    redirect("/login")
-  }
-
-  return <div></div>;
+  redirect("https://github.com/analogdotnow/Analog");
 }
