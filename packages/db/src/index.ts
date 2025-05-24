2c0084d2c9dde78db983b2368114ab0a7bb91142
iboughtbed <122831015+iboughtbed@users.noreply.github.com>
2025-05-25 00:52:43 +0500

    fix(db): cache drizzle to prevent new connections on every HMR update (#48)

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,28 @@
 import "server-only";
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+
 import { env } from "@repo/env/server";
 import * as schema from "./schema";
 
-// Disable prefetch as it is not supported for Supabase "Transaction" pool mode
-const client = postgres(env.DATABASE_URL, { prepare: false });
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: postgres.Sql | undefined;
+};
 
-export const db = drizzle(client, { schema, casing: "snake_case" });
+/**
+ * Disable prefetch as it is not supported for Supabase "Transaction" pool mode.
+ * @see https://supabase.com/docs/guides/database/connecting-to-postgres#supavisor-transaction-mode
+ */
+const conn = globalForDb.conn ?? postgres(env.DATABASE_URL, { prepare: false });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+/**
+ * Automatically map camelCase to snake_case in the database.
+ * @see https://orm.drizzle.team/docs/sql-schema-declaration#camel-and-snake-casing
+ */
+export const db = drizzle(conn, { schema, casing: "snake_case" });
