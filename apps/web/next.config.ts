559b8167dd135850d77a05cc07b752713f99bc04
Jean P.D. Meijer <9619050+JeanMeijer@users.noreply.github.com>
2025-06-23 12:50:45 +0200

    chore(web): add discord redirect (#133)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 import withSimpleAnalytics from "@simpleanalytics/next/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/K3AsABDKUm",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default withSimpleAnalytics(nextConfig);
